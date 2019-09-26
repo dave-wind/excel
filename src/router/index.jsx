55aa80e6dda0f9e3793395713f33dfd1c00c4a48
dave <byrand117@qq.com>
2019-09-26 13:46:26 +0800

    Update index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,Redirect} from 'react-router-dom';
 import { AsyncComponent } from '../components/AsyncComponent.jsx'
 
 const Index = AsyncComponent(() => import('../pages/index.jsx'));
